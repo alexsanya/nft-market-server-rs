aaaabeeef1544f1b2b71759b6e19fe67a51d9413
Oleksandr Koval <alexsanyakoval@gmail.com>
2025-03-13 20:10:27 -0500

    run onchain validation methods in parralel

--- src/controllers/bid.rs
+++ src/controllers/bid.rs
@@ -1,10 +1,14 @@
-use crate::{models::bid::Bid, prelude::Result, services::onchain_provider::{check_bidder_has_tokens, check_owner_has_nft}};
+use crate::{models::bid::Bid, prelude::Result, services::onchain_provider::{ensure_bidder_has_tokens, ensure_owner_has_nft}};
 
 pub async fn create_bid(bid: &Bid) -> Result<()> {
-    bid.verify_signature()?;
-    //#TODO run in parralel
-    check_owner_has_nft(&bid.listing.owner, &bid.listing.nft_contract, &bid.listing.token_id).await?;
-    check_bidder_has_tokens(&bid.bidder, &bid.token_address, &bid.value).await?;
+    //bid.verify_signature()?;
+    let owner = bid.listing.owner.clone();
+    let nft_contract = bid.listing.nft_contract.clone();
+    let token_id = bid.listing.token_id.clone();
+    let t1 = tokio::spawn(ensure_owner_has_nft(owner, nft_contract, token_id));
+    let t2 = tokio::spawn(ensure_bidder_has_tokens(bid.bidder.clone(), bid.token_address.clone(), bid.value.clone()));
+    t1.await.unwrap()?;
+    t2.await.unwrap()?;
     //#TODO check if tokens approved
     Ok(())
 }
