03ba3035f8d929db3ddf50081b77de11cf29ae73
Oleksandr Koval <oleksandrkoval@ballastlane.com>
2025-03-14 17:58:11 -0500

    Revert "run onchain validation methods in parralel"
    This reverts commit f3e502e6eac95cc46cd5e0ea50a7a814588ce89e.

--- src/controllers/listing.rs
+++ src/controllers/listing.rs
@@ -1,9 +1,9 @@
-use crate::{models::listing::Listing, repositories::listing::{get_all, save_listing}, services::onchain_provider::ensure_owner_has_nft};
+use crate::{models::listing::Listing, repositories::listing::{get_all, save_listing}, services::onchain_provider::check_owner_has_nft};
 use crate::prelude::*;
 
 pub async fn create_listing(listing: &Listing) -> Result<()> {
     listing.verify_signature()?;
-    ensure_owner_has_nft(listing.owner.clone(), listing.nft_contract.clone(), listing.token_id.clone()).await?;
+    check_owner_has_nft(&listing.owner, &listing.nft_contract, &listing.token_id).await?;
     save_listing(listing).await?;
     Ok(())
 }
