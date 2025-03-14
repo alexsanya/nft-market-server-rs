8b21350207ca2336006d4e0d46456e168df5fc68
Oleksandr Koval <alexsanyakoval@gmail.com>
2025-03-14 17:58:11 -0500

    Revert "run onchain validation methods in parralel"
    This reverts commit aaaabeeef1544f1b2b71759b6e19fe67a51d9413.

--- src/controllers/bid.rs
+++ src/controllers/bid.rs
@@ -2,13 +2,9 @@ use crate::{models::bid::Bid, prelude::Result, repositories::bids::{get_all, sav
 
 pub async fn create_bid(bid: &Bid) -> Result<()> {
     bid.verify_signature()?;
-    let owner = bid.listing.owner.clone();
-    let nft_contract = bid.listing.nft_contract.clone();
-    let token_id = bid.listing.token_id.clone();
-    let t1 = tokio::spawn(ensure_owner_has_nft(owner, nft_contract, token_id));
-    let t2 = tokio::spawn(ensure_bidder_has_tokens(bid.bidder.clone(), bid.token_address.clone(), bid.value.clone()));
-    t1.await.unwrap()?;
-    t2.await.unwrap()?;
+    //#TODO run in parralel
+    check_owner_has_nft(&bid.listing.owner, &bid.listing.nft_contract, &bid.listing.token_id).await?;
+    check_bidder_has_tokens(&bid.bidder, &bid.token_address, &bid.value).await?;
     save_bid(bid).await?;
     //#TODO check if tokens approved
     Ok(())
