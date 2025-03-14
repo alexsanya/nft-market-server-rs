a34c2f40e6e2d5433954dba025dd07f7177389d6
Oleksandr Koval <oleksandrkoval@ballastlane.com>
2025-03-14 18:13:51 -0500

    use join instead of spawn

--- src/controllers/bid.rs
+++ src/controllers/bid.rs
@@ -1,10 +1,15 @@
+use tokio::join;
+
 use crate::{models::bid::Bid, prelude::Result, repositories::bids::{get_all, save_bid}, services::onchain_provider::{ensure_bidder_has_tokens, ensure_owner_has_nft}};
 
 pub async fn create_bid(bid: &Bid) -> Result<()> {
     bid.verify_signature()?;
-    //#TODO run in parralel
-    check_owner_has_nft(&bid.listing.owner, &bid.listing.nft_contract, &bid.listing.token_id).await?;
-    check_bidder_has_tokens(&bid.bidder, &bid.token_address, &bid.value).await?;
+    let (nft_check, tokens_check)  = join!(
+        ensure_owner_has_nft(&bid.listing.owner, &bid.listing.nft_contract, &bid.listing.token_id),
+        ensure_bidder_has_tokens(&bid.bidder, &bid.token_address, &bid.value)
+    );
+    nft_check?;
+    tokens_check?;
     save_bid(bid).await?;
     //#TODO check if tokens approved
     Ok(())
