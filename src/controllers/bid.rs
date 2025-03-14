849d14173b8abbb3fa7da5ac9f9122e36accb3af
Oleksandr Koval <oleksandrkoval@ballastlane.com>
2025-03-13 19:50:56 -0500

    add onchain validation for bids

--- src/controllers/bid.rs
+++ src/controllers/bid.rs
@@ -1,6 +1,10 @@
-use crate::{models::bid::Bid, prelude::Result};
+use crate::{models::bid::Bid, prelude::Result, services::onchain_provider::{check_bidder_has_tokens, check_owner_has_nft}};
 
 pub async fn create_bid(bid: &Bid) -> Result<()> {
     bid.verify_signature()?;
+    //#TODO run in parralel
+    check_owner_has_nft(&bid.listing.owner, &bid.listing.nft_contract, &bid.listing.token_id).await?;
+    check_bidder_has_tokens(&bid.bidder, &bid.token_address, &bid.value).await?;
+    //#TODO check if tokens approved
     Ok(())
 }
