5c6a248d0cf8aca0ce3509ed5c6740f4df8a2d2c
Oleksandr Koval <oleksandrkoval@ballastlane.com>
2025-03-14 19:40:45 -0500

    extract contract addresses to settings

--- src/controllers/bid.rs
+++ src/controllers/bid.rs
@@ -1,17 +1,19 @@
 use tokio::join;
-
-use crate::{models::bid::Bid, prelude::Result, repositories::bids::{get_all, save_bid}, services::onchain_provider::{ensure_bidder_has_tokens, ensure_owner_has_nft}};
+use crate::{models::bid::Bid, prelude::Result, repositories::bids::{get_all, save_bid}, services::onchain_provider::{ensure_bidder_has_tokens, ensure_erc20_allowance, ensure_owner_has_nft, esnure_erc712_allowance}};
 
 pub async fn create_bid(bid: &Bid) -> Result<()> {
     bid.verify_signature()?;
-    let (nft_check, tokens_check)  = join!(
+    let (nft_ownership_check, nft_allowance_check, tokens_check, allowance_check)  = join!(
         ensure_owner_has_nft(&bid.listing.owner, &bid.listing.nft_contract, &bid.listing.token_id),
-        ensure_bidder_has_tokens(&bid.bidder, &bid.token_address, &bid.value)
+        esnure_erc712_allowance(&bid.listing.owner, &bid.listing.nft_contract),
+        ensure_bidder_has_tokens(&bid.bidder, &bid.token_address, &bid.value),
+        ensure_erc20_allowance(&bid.bidder, &bid.token_address, &bid.value)
     );
-    nft_check?;
+    nft_ownership_check?;
+    nft_allowance_check?;
     tokens_check?;
+    allowance_check?;
     save_bid(bid).await?;
-    //#TODO check if tokens approved
     Ok(())
 }
 
